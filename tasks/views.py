802c3cd54d90fa49837b1740ba172b0a32cec0f6
matinshafie <matinshafie88@gmail.com>
2026-06-05 21:07:56 +0330

    created task viewset

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import TaskSerializer
+from .models import Task
 
 # Create your views here.
+class TaskViewSet(ModelViewSet):
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
