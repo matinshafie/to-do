7415b60def3499f123db7a076179c0975b078fbe
matinshafie <matinshafie88@gmail.com>
2026-06-05 17:15:37 +0330

    registered the admin model

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from .models import Task
 
 # Register your models here.
+@admin.register(Task)
 class TasksAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'created_at', 'completed', 'user')
     list_filter = ('completed', 'user')
