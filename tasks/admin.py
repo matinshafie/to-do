e97028913567669c093674c722def661c6a233e9
matinshafie <matinshafie88@gmail.com>
2026-06-05 17:11:25 +0330

    created the admin pannel for tasks

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+class TasksAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'created_at', 'completed', 'user')
+    list_filter = ('completed', 'user')
+    search_fields = ('title', 'description')
