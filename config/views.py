57b7c7707b3fb02c98ddcd4ed2003014643c0e6d
matinshafie <matinshafie88@gmail.com>
2026-06-10 13:14:34 +0330

    implemented logout

--- config/views.py
+++ config/views.py
@@ -1,6 +1,9 @@
+from rest_framework.views import APIView
+from rest_framework import status
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.response import Response
 from rest_framework.request import Request
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class CookieTokenObtainPairView(TokenObtainPairView):
@@ -25,3 +28,22 @@ class CookieTokenObtainPairView(TokenObtainPairView):
             del response.data['access']
 
         return super().finalize_response(request, response, *args, **kwargs)
+
+
+class LogoutView(APIView):
+    def post(self, request: Request):
+        # 1. Attempt to blacklist the refresh token if using the blacklist app
+        try:
+            refresh_token = request.COOKIES.get('refresh_token')
+            if refresh_token:
+                token = RefreshToken(refresh_token)
+                token.blacklist()
+        except Exception:
+            pass 
+
+        # 2. Create response and clear cookies
+        response = Response({"message": "Successfully logged out"}, status=status.HTTP_200_OK)
+        response.delete_cookie('access_token')
+        response.delete_cookie('refresh_token')
+        
+        return response
