d308df6eb44f04f3178859bb6eb6111eba2eed70
matinshafie <matinshafie88@gmail.com>
2026-06-21 00:52:15 +0200

    added uuid field to list model

--- tasks/models.py
+++ tasks/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.conf import settings
+from uuid import uuid4
 
 
 class List(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=255)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='lists'
