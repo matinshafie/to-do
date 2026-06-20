f3983f903f8389bae88d9be50f4815e98339812c
matinshafie <matinshafie88@gmail.com>
2026-06-21 00:48:11 +0200

    implemented the list for tasks

--- tasks/models.py
+++ tasks/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.conf import settings
 
+
+class List(models.Model):
+    title = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='lists'
+        )
+    created_at = models.DateTimeField(auto_now_add=True)
+
 class Task(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=500, null=True, blank=True)
@@ -12,3 +20,4 @@ class Task(models.Model):
     repeat = models.PositiveIntegerField(null=True, blank=True)
     due_date = models.DateField(null=True, blank=True)
     completed_at = models.DateTimeField(null=True, blank=True)
+    list = models.ForeignKey(List, models.CASCADE, related_name='tasks', blank=True, null=True)
