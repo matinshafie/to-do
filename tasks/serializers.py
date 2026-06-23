dd41db3e85ab59cd46acdb6375cc9ddd75f2f541
matinshafie <matinshafie88@gmail.com>
2026-06-24 00:24:43 +0330

    merged serializer classes into one and fixed the views

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -2,37 +2,37 @@ from rest_framework import serializers
 from .models import Task, List
 from django.utils import timezone
 
-
-class CreateTaskSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Task
-        fields = '__all__'
-        read_only_fields = ['user', 'completed', 'completed_at', 'list']
-
-    def create(self, validated_data:dict):
-        repeat = validated_data.get('repeat')
-        due_date = validated_data.get('due_date')
-
-        if repeat and not due_date:
-            validated_data['due_date'] = timezone.now().date()
-
-        return super().create(validated_data)
-
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = '__all__'
-        read_only_fields = ['user', 'completed_at', 'list']
+        fields = 'id', 'title', 'description', 'created_at', 'completed', 'repeat', 'due_date', 'list'
 
-class UpdateTaskSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Task
-        fields = ['title', 'description', 'completed', 'repeat', 'due_date', 'list']
-        read_only_fields = ['list']
+    def get_fields(self):
+        fields = super().get_fields()
+        if self.context.get('request').method == 'POST':
+            fields.pop('completed', None)
+            fields.pop('list', None)
+        
+        elif self.context.get('request').method in ('PUT', 'PATCH'):
+            fields.pop('completed', None)
+            fields.pop('user', None)
+            fields.pop('list', None)
+
+        return fields
+
+    def validate(self, attrs):
+        attrs['user'] = self.context('request').user
+
+        attrs['list'] = List.objects.filter(pk=self.context.get('list_pk'), user=attrs['user']).first()
+
+        if attrs['list']:
+            raise 
+
+        return super().validate(attrs)
 
     def update(self, instance: Task, validated_data: dict):
-        completed = validated_data.get('completed', instance.completed)
-        repeat = validated_data.get('repeat', instance.repeat)
+        completed = validated_data.get('completed')
+        repeat = validated_data.get('repeat')
 
         if completed and repeat:
             if not validated_data.get('due_date'):
@@ -42,7 +42,14 @@ class UpdateTaskSerializer(serializers.ModelSerializer):
 
         return super().update(instance, validated_data)
 
+    def create(self, validated_data:dict):
+        repeat = validated_data.get('repeat')
+        due_date = validated_data.get('due_date')
 
+        if repeat and not due_date:
+            validated_data['due_date'] = timezone.now().date()
+
+        return super().create(validated_data)
 
 class ListSerializer(serializers.ModelSerializer):
     class Meta:
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,7 @@
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.mixins import RetrieveModelMixin, \
     ListModelMixin, DestroyModelMixin, UpdateModelMixin
-from .serializers import TaskSerializer, CreateTaskSerializer, UpdateTaskSerializer, ListSerializer
+from .serializers import TaskSerializer, ListSerializer
 from .models import Task, List
 from django.utils import timezone
 
@@ -11,13 +11,6 @@ class TaskViewSet(ModelViewSet):
         
     def get_queryset(self):
         return Task.objects.filter(user=self.request.user, list=None)
-
-    def get_serializer_class(self):
-        if self.action == 'create':
-            return CreateTaskSerializer
-        elif self.action in ['update', 'partial_update']:
-            return UpdateTaskSerializer
-        return TaskSerializer
     
     def perform_create(self, serializer:TaskSerializer):
         serializer.save(user=self.request.user)
@@ -26,6 +19,7 @@ class MyDayTaskViewSet(
     RetrieveModelMixin, ListModelMixin, 
     DestroyModelMixin, UpdateModelMixin, GenericViewSet
     ):
+    serializer_class = TaskSerializer
 
     def get_queryset(self):
         return Task.objects.filter(user=self.request.user, due_date=timezone.localdate())
@@ -33,11 +27,6 @@ class MyDayTaskViewSet(
     def perform_create(self, serializer:TaskSerializer):
         serializer.save(user=self.request.user)
 
-    def get_serializer_class(self):
-        if self.action in ['update', 'partial_update']:
-            return UpdateTaskSerializer
-        return TaskSerializer
-    
 
 class ListViewSet(ModelViewSet):
     serializer_class = ListSerializer
@@ -49,15 +38,10 @@ class ListViewSet(ModelViewSet):
         serializer.save(user=self.request.user)
 
 class TaskListViewSet(ModelViewSet):
+    serializer_class = TaskSerializer
+
     def get_queryset(self):
         return Task.objects.filter(user=self.request.user, list_id=self.kwargs.get('list_pk'))
-    
-    def get_serializer_class(self):
-        if self.action == 'create':
-            return CreateTaskSerializer
-        elif self.action in ['update', 'partial_update']:
-            return UpdateTaskSerializer
-        return TaskSerializer
     
     def perform_create(self, serializer:TaskSerializer):
         serializer.save(user=self.request.user)
