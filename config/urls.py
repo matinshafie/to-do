4eccd04fd8e04df40aeb9fc3c2a8b88d52a5c1d2
matinshafie <matinshafie88@gmail.com>
2026-07-04 15:13:10 +0200

    fixed the commented code

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,6 @@ from .views import CookieTokenObtainPairView, CookieTokenRefreshView
 urlpatterns = [
     path("jwt/create/", CookieTokenObtainPairView.as_view()),
     path("jwt/refresh/", CookieTokenRefreshView.as_view()),
-    # path("", include("djoser.urls")),
-    # path("", include("djoser.urls.jwt")),
+    path("", include("djoser.urls")),
+    path("", include("djoser.urls.jwt")),
 ]
