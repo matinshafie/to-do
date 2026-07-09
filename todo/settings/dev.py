75d8839cbc6c56817c0e355ac1884461eaad5d6e
matinshafie <matinshafie88@gmail.com>
2026-07-09 14:11:04 +0330

    implemented the settings for production and developments

---
+++ todo/settings/dev.py
@@ -0,0 +1,45 @@
+from .base import *  # noqa: F401,F403
+from os import getenv
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+INSTALLED_APPS += [
+    'debug_toolbar',
+    'drf_spectacular_sidecar',
+    'corsheaders',
+]
+
+MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+] + MIDDLEWARE
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
+
+SPECTACULAR_SETTINGS = {
+    **SPECTACULAR_SETTINGS,
+    'SWAGGER_UI_DIST': 'SIDECAR',
+    'SWAGGER_UI_FAVICON_HREF': 'SIDECAR',
+    'REDOC_DIST': 'SIDECAR',
+}
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': getenv('MYSQL_NAME'),
+        'USER': getenv('MYSQL_USER'),
+        'HOST': getenv('MYSQL_HOST'),
+        'PASSWORD': getenv('MYSQL_PASSWORD'),
+    }
+}
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
+
+# Not HTTPS locally, so cookies can't be marked secure
+AUTH_COOKIE_SECURE = False
---
+++ todo/settings/prod.py
@@ -0,0 +1,51 @@
+from .base import *  # noqa: F401,F403
+from os import getenv
+
+DEBUG = False
+
+ALLOWED_HOSTS = getenv('DJANGO_ALLOWED_HOSTS', '').split(',')
+
+INSTALLED_APPS += [
+    'corsheaders',
+]
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': getenv('MYSQL_NAME'),
+        'USER': getenv('MYSQL_USER'),
+        'HOST': getenv('MYSQL_HOST'),
+        'PASSWORD': getenv('MYSQL_PASSWORD'),
+        'CONN_MAX_AGE': 60,
+    }
+}
+
+CORS_ALLOWED_ORIGINS = getenv('CORS_ALLOWED_ORIGINS', '').split(',')
+
+# --- Security hardening ---
+AUTH_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+SECURE_SSL_REDIRECT = True
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Logs to stdout so Docker/Gunicorn/systemd can capture them
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {'class': 'logging.StreamHandler'},
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+}
